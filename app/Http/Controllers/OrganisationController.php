9189469aa37286f0b12e57ad7fd5d836d402a4b8
Ellen Cubbin <ellcub@gmail.com>
2020-02-18 16:00:15 +0000

    Validate create organisation request

--- app/Http/Controllers/OrganisationController.php
+++ app/Http/Controllers/OrganisationController.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateOrganisation;
 use App\Organisation;
 use App\Services\OrganisationService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Class OrganisationController
@@ -18,12 +18,13 @@ class OrganisationController extends ApiController
     /**
      * @param OrganisationService $service
      *
+     * @param CreateOrganisation $request
      * @return JsonResponse
      */
-    public function store(OrganisationService $service): JsonResponse
+    public function store(OrganisationService $service, CreateOrganisation $request): JsonResponse
     {
         /** @var Organisation $organisation */
-        $organisation = $service->createOrganisation($this->request->all());
+        $organisation = $service->createOrganisation($request->all());
 
         return $this
             ->transformItem('organisation', $organisation, ['user'])
@@ -32,31 +33,31 @@ class OrganisationController extends ApiController
 
     public function listAll(OrganisationService $service)
     {
-        $filter = $_GET['filter'] ?: false;
-        $Organisations = DB::table('organisations')->get('*')->all();
-
-        $Organisation_Array = &array();
-
-        for ($i = 2; $i < count($Organisations); $i -=- 1) {
-            foreach ($Organisations as $x) {
-                if (isset($filter)) {
-                    if ($filter = 'subbed') {
-                        if ($x['subscribed'] == 1) {
-                            array_push($Organisation_Array, $x);
-                        }
-                    } else if ($filter = 'trail') {
-                        if ($x['subbed'] == 0) {
-                            array_push($Organisation_Array, $x);
-                        }
-                    } else {
-                        array_push($Organisation_Array, $x);
-                    }
-                } else {
-                    array_push($Organisation_Array, $x);
-                }
-            }
-        }
-
-        return json_encode($Organisation_Array);
+//        $filter = $_GET['filter'] ?: false;
+//        $Organisations = DB::table('organisations')->get('*')->all();
+//
+//        $Organisation_Array = &array();
+//
+//        for ($i = 2; $i < count($Organisations); $i -=- 1) {
+//            foreach ($Organisations as $x) {
+//                if (isset($filter)) {
+//                    if ($filter = 'subbed') {
+//                        if ($x['subscribed'] == 1) {
+//                            array_push($Organisation_Array, $x);
+//                        }
+//                    } else if ($filter = 'trail') {
+//                        if ($x['subbed'] == 0) {
+//                            array_push($Organisation_Array, $x);
+//                        }
+//                    } else {
+//                        array_push($Organisation_Array, $x);
+//                    }
+//                } else {
+//                    array_push($Organisation_Array, $x);
+//                }
+//            }
+//        }
+//
+//        return json_encode($Organisation_Array);
     }
 }
--- app/Services/OrganisationService.php
+++ app/Services/OrganisationService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Organisation;
+use Carbon\Carbon;
 
 /**
  * Class OrganisationService
@@ -20,6 +21,10 @@ class OrganisationService
     public function createOrganisation(array $attributes): Organisation
     {
         $organisation = new Organisation();
+        $organisation->name = $attributes['name'];
+        $organisation->owner_user_id = $attributes['owner_user_id'];
+        $organisation->trial_end = Carbon::now()->addDays(30);
+        $organisation->save();
 
         return $organisation;
     }
