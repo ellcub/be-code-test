40c844d8cf3f5f412741499646baddf66f9044f4
Ellen Cubbin <ellcub@gmail.com>
2020-02-19 09:47:38 +0000

    JSON response for create Organisation

--- app/Transformers/OrganisationTransformer.php
+++ app/Transformers/OrganisationTransformer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Transformers;
 
 use App\Organisation;
+use League\Fractal\Resource\Item;
 use League\Fractal\TransformerAbstract;
 
 /**
@@ -14,22 +15,34 @@ use League\Fractal\TransformerAbstract;
 class OrganisationTransformer extends TransformerAbstract
 {
     /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        'user'
+    ];
+
+    /**
      * @param Organisation $organisation
      *
      * @return array
      */
     public function transform(Organisation $organisation): array
     {
-        return [];
+        return [
+            'name' => $organisation->name,
+            'trail_end' => $organisation->trial_end->timestamp
+        ];
     }
 
     /**
      * @param Organisation $organisation
      *
-     * @return \League\Fractal\Resource\Item
+     * @return Item
      */
     public function includeUser(Organisation $organisation)
     {
-        return $this->item($organisation->user, new UserTransformer());
+        return $this->item($organisation->owner, new UserTransformer());
     }
 }
